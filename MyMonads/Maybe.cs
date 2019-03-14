d525e24c8f695425c3855129a650855ebf616f5e
Michał Włodarczyk <mwlodarczyk@forcom.com.pl>
2019-03-14 16:30:46 +0100

    Compare "Get manager's manager name" with Maybe monad and without it

--- MyMonads/Maybe.cs
+++ MyMonads/Maybe.cs
@@ -1,43 +1,98 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 
 namespace MyMonads
 {
 	public class Maybe<TValue>
 	{
-		public TValue Value { get; private set; }
-		public bool HasValue { get; private set; }
+		private readonly TValue value;
+		private readonly bool hasValue;
 
 		public Maybe(TValue value)
 		{
-			Value = value;
-			HasValue = true;
+			this.value = value;
+			this.hasValue = true;
 		}
 
-		public Maybe()
+		private Maybe()
 		{
-			HasValue = false;
-		} 
+			this.hasValue = false;
+		}
+
+		public Maybe<TValue> Bind(Func<TValue, Maybe<TValue>> bindFn)
+		{
+			return hasValue ? bindFn(this.value) : None();
+		}
+
+		public T Match<T>(Func<TValue, T> valueFn, Func<T> nothingFn)
+		{
+			return this.hasValue ? valueFn(this.value) : nothingFn();
+		}
+
+		public static Maybe<TValue> None()
+		{
+			return new Maybe<TValue>();
+		}
 	}
 
 	[TestFixture]
 	public class MaybeTests
 	{
-		[Test]
-		public void Maybe_HasValue()
+		class EmployeeOld
 		{
-			var m = new Maybe<int>(2);
+			public string Name;
+			public EmployeeOld Manager;
+		}
 
-			Assert.AreEqual(2, m.Value);
-			Assert.True(m.HasValue);
+		class EmployeeNew
+		{
+			public string Name;
+			public Maybe<EmployeeNew> Manager;
+		}
+
+		static string GetManagerManagerNameOld(EmployeeOld employee)
+		{
+			if (employee.Manager == null)
+			{
+				return null;
+			}
+			if (employee.Manager.Manager == null)
+			{
+				return null;
+			}
+
+			return employee.Manager.Manager.Name;
+		}
+
+		static string GetManagerManagerNameNew(EmployeeNew employee)
+		{
+			 return employee.Manager
+				.Bind(e => e.Manager)
+				.Match(e => e.Name, () => null);
 		}
 
 		[Test]
-		public void Maybe_DoesntHaveValue()
+		public void WriteEmployeeManagerManagerWithoutMonads()
 		{
-			var m = new Maybe<int>();
+			var m0 = new EmployeeOld { Name = "RB" };
+			var m1 = new EmployeeOld { Name = "MJ", Manager = m0 };
+			var m2 = new EmployeeOld { Name = "MT", Manager = m1 };
 
-			Assert.False(m.HasValue);
+			var directorName = GetManagerManagerNameOld(m2);
+
+			Assert.AreEqual("RB", directorName);
 		}
 
+		[Test]
+		public void WriteEmployeeManagerManagerWithMonadsExample()
+		{
+			var m0 = new EmployeeNew { Name = "RB", Manager = Maybe<EmployeeNew>.None() };
+			var m1 = new EmployeeNew { Name = "MJ", Manager = new Maybe<EmployeeNew>(m0) };
+			var m2 = new EmployeeNew { Name = "MT", Manager = new Maybe<EmployeeNew>(m1) };
+
+			var directorName = GetManagerManagerNameNew(m2);
+
+			Assert.AreEqual("RB", directorName);
+		}
 	}
 }
