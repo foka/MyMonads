99bde7a68bd6dafe295a7cebf794c740e74bb765
Michał Włodarczyk <mwlodarczyk@forcom.com.pl>
2019-03-14 15:16:31 +0100

    Init repo

---
+++ MyMonads/Maybe.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace MyMonads
+{
+	public class Maybe
+	{
+		
+	}
+
+	[TestFixture]
+	public class MaybeTests
+	{
+		[Test]
+		public void Yo()
+		{
+			
+		}
+	}
+}
