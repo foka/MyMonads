32d214ad18c45a0164f0eb3e886184bf363603cb
Michał Włodarczyk <mwlodarczyk@forcom.com.pl>
2019-03-14 15:23:53 +0100

    Constructor

--- MyMonads/Maybe.cs
+++ MyMonads/Maybe.cs
@@ -2,18 +2,42 @@
 
 namespace MyMonads
 {
-	public class Maybe
+	public class Maybe<TValue>
 	{
-		
+		public TValue Value { get; private set; }
+		public bool HasValue { get; private set; }
+
+		public Maybe(TValue value)
+		{
+			Value = value;
+			HasValue = true;
+		}
+
+		public Maybe()
+		{
+			HasValue = false;
+		} 
 	}
 
 	[TestFixture]
 	public class MaybeTests
 	{
 		[Test]
-		public void Yo()
+		public void Maybe_HasValue()
 		{
-			
+			var m = new Maybe<int>(2);
+
+			Assert.AreEqual(2, m.Value);
+			Assert.True(m.HasValue);
 		}
+
+		[Test]
+		public void Maybe_DoesntHaveValue()
+		{
+			var m = new Maybe<int>();
+
+			Assert.False(m.HasValue);
+		}
+
 	}
 }
